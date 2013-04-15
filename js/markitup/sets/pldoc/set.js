965bda9a0f700a8a4c8ab1696fe180a1fb2a4ff4
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2013-04-15 09:12:21 +0200

    Started creating a real PlDoc mode

--- js/markitup/sets/pldoc/set.js
+++ js/markitup/sets/pldoc/set.js
@@ -14,26 +14,23 @@ pldoc_settings = {
 	previewParserPath:	'/markitup/preview/pldoc',
 	onShiftEnter:		{keepDefault:false, openWith:'\n\n'},
 	markupSet: [
-		{name:'First Level Heading', key:'1', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '=') } },
-		{name:'Second Level Heading', key:'2', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '-') } },
-		{name:'Heading 3', key:'3', openWith:'### ', placeHolder:'Your title here...' },
-		{name:'Heading 4', key:'4', openWith:'#### ', placeHolder:'Your title here...' },
-		{name:'Heading 5', key:'5', openWith:'##### ', placeHolder:'Your title here...' },
-		{name:'Heading 6', key:'6', openWith:'###### ', placeHolder:'Your title here...' },
+		{name:'Heading 1', key:'1', openWith:'---+ ', placeHolder:'Your title here...' },
+		{name:'Heading 2', key:'2', openWith:'---++ ', placeHolder:'Your title here...' },
+		{name:'Heading 3', key:'3', openWith:'---+++ ', placeHolder:'Your title here...' },
 		{separator:'---------------' },
-		{name:'Bold', key:'B', openWith:'**', closeWith:'**'},
+		{name:'Bold',   key:'B', openWith:'*', closeWith:'*'},
 		{name:'Italic', key:'I', openWith:'_', closeWith:'_'},
+		{name:'Code',   key:'C', openWith:'=', closeWith:'='},
 		{separator:'---------------' },
-		{name:'Bulleted List', openWith:'- ' },
-		{name:'Numeric List', openWith:function(markItUp) {
-			return markItUp.line+'. ';
+		{name:'Bulleted List', openWith:'  - ', multiline:true },
+		{name:'Numeric List', multiline:true, openWith:function(markItUp) {
+			return "  "+markItUp.line+'. ';
 		}},
 		{separator:'---------------' },
 		{name:'Picture', key:'P', replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!] "[![Title]!]")'},
 		{name:'Link', key:'L', openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder:'Your text to link here...' },
 		{separator:'---------------'},
-		{name:'Quotes', openWith:'> '},
-		{name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},
+		{name:'Code Block', openBlockWith:'  ==\n', closeBlockWith:'\n  ==', openWith:'  '},
 		{separator:'---------------'},
 		{name:'Preview', call:'preview', className:"preview"}
 	]
